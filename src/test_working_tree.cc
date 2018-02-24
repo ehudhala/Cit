9a8e968ff341077734f72d612c8f5fbc972101ec
Ehud <ehudhala@gmail.com>
2018-02-24 15:02:19 +0200

    changed file names to informative names

--- src/test_working_tree.cc
+++ src/test_working_tree.cc
@@ -6,29 +6,29 @@ using namespace cit;
 using namespace inmemory;
 
 TEST(working_tree_list, returns_names) {
-    working_tree_t tree{{{"ab", "bc"}, {"cd", "ef"}}};
-    std::vector<name_t> expected{"ab", "cd"};
+    working_tree_t tree{{{"name", "content"}, {"name2", "content2"}}};
+    std::vector<name_t> expected{"name", "name2"};
     EXPECT_EQ(expected, tree.list());
 }
 
 TEST(working_tree_read, existing) {
-    working_tree_t tree{{{"ab", "bc"}}};
-    auto content = tree.read("ab");
+    working_tree_t tree{{{"name", "content"}}};
+    auto content = tree.read("name");
     ASSERT_TRUE(bool(content));
-    EXPECT_EQ("bc", *content);
+    EXPECT_EQ("content", *content);
 }
 
 TEST(working_tree_read, non_existing) {
-    working_tree_t tree{{{"ab", "bc"}}};
-    ASSERT_FALSE(bool(tree.read("non_existing")));
+    working_tree_t tree{{{"name", "content"}}};
+    EXPECT_FALSE(bool(tree.read("non_existing")));
 }
 
 TEST(working_tree_contains, existing) {
-    working_tree_t tree{{{"ab", "bc"}}};
-    EXPECT_TRUE(tree.contains("ab"));
+    working_tree_t tree{{{"name", "content"}}};
+    EXPECT_TRUE(tree.contains("name"));
 }
 
 TEST(working_tree_contains, non_existing) {
-    working_tree_t tree{{{"ab", "bc"}}};
+    working_tree_t tree{{{"name", "content"}}};
     EXPECT_FALSE(tree.contains("non_existing"));
 }
